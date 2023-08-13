d90b3617ef50897d1d489b8cabab0907fdb9ca6a
CalvinWan <t110820058@ntut.org.tw>
2023-08-14 04:02:54 +0800

    [Ex1, Iteration3, Step1] Write MyBoardTestCase and create_a_myboard_generates_a_myboard_created_domain_event test method that cannot be compiled.

---
+++ board/src/test/java/ntut/csie/sslab/kanban/board/entity/MyBoardTestCase2.java
@@ -0,0 +1,14 @@
+package ntut.csie.sslab.kanban.board.entity;
+
+import ntut.csie.sslab.kanban.board.entity.event.MyBoardCreated;
+import org.junit.jupiter.api.Test;
+
+public class MyBoardTestCase2 {
+    @Test
+    public void create_a_myboard_generates_a_myboard_created_domain_event(){
+        MyBoard2 board = new MyBoard2("", "", "");
+
+        assertEquals(1,board.getDomainEvents().size());
+        assertEquals(MyBoardCreated.class, board.getDomainEvents().get(0).getClass());
+    }
+}
